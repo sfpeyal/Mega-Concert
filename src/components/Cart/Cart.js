bd48d3b251db4cb818f3aca417dc848850435821
shaikh foysal peyal <sfpeyal@gmail.com>
2021-09-26 22:38:08 +0600

    last push

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,30 +2,35 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserPlus, faDollarSign } from '@fortawesome/free-solid-svg-icons'
 import './Cart'
-import Name from '../Name/Name';
+
 
 const Cart = (props) => {
+    console.log(props.cart);
     const { length } = props.cart
     const element = <FontAwesomeIcon icon={faUserPlus} />
     const element2 = <FontAwesomeIcon icon={faDollarSign} />
     const { cart } = props;
     let balance = 0;
+    let showName = []
     for (const singer of cart) {
         balance = balance + parseInt(singer.bankBalance);
-    }
-    let newSinger = '';
-    for (const singer of cart) {
-        newSinger = singer.name;
-
-
+        showName.push(singer)
     }
     return (
         <div>
             <h3>{element} Singer added: {length}</h3>
             <p>{element2} Bank balance: ${balance}</p>
-            <Name name={newSinger}></Name>
+
+            {
+                showName.map((item) => (
+                    <div>
+                        <h4 key={item.id}>{item.name}</h4>
+                    </div>
+                ))
+            }
+
+
         </div>
     );
-};
-
+}
 export default Cart;
